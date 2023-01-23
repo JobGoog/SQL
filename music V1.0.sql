143739133a5b01e9ce6e0aaa6d28c1ddb7fe91a6
JobGoog <roro425052@gmail.com>
2023-01-23 16:44:42 +0300

    done

---
+++ music V1.0.sql
@@ -0,0 +1,46 @@
+create table if not EXISTS genre (
+id SERIAL PRIMARY key,
+name VARCHAR(50) not null
+);
+
+create table if not EXISTS artist (
+id SERIAL PRIMARY key,
+nick VARCHAR(50) not null
+);
+
+CREATE TABLE IF NOT EXISTS genre_list (
+	id SERIAL primary key,
+	genre_id INTEGER not null REFERENCES genre(id),
+	artist_id INTEGER not null REFERENCES artist(id)
+);
+
+create table if not EXISTS album (
+id SERIAL PRIMARY key,
+name VARCHAR(50) not null,
+year VARCHAR(50) not null
+);
+
+CREATE TABLE IF NOT EXISTS album_list (
+	id SERIAL primary key,
+	album_id INTEGER not null REFERENCES album(id),
+	artist_id INTEGER not null REFERENCES artist(id)
+);
+
+create table if not exists song (
+id SERIAL primary key,
+name VARCHAR(50) not null,
+year VARCHAR(50) not null,
+album_id INTEGER not null references album(id)
+);
+
+create table if not EXISTS playlist (
+id SERIAL PRIMARY key,
+name VARCHAR(50) not null,
+year VARCHAR (50) not null
+);
+
+CREATE TABLE IF NOT EXISTS playlist_list (
+	id SERIAL primary key,
+	playlist_id INTEGER not null REFERENCES playlist(id),
+	song_id INTEGER not null REFERENCES song(id)
+);
